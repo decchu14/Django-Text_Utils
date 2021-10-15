db08504dd737f98455afb84b900c256ce522e693
Dashline Dsouza <42490741+decchu14@users.noreply.github.com>
2021-10-15 15:32:50 +0530

    Update settings.py

--- Text_Utils/settings.py
+++ Text_Utils/settings.py
@@ -1,3 +1,5 @@
+from decouple import config
+
 """
 Django settings for Text_Utils project.
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--$z*^p_euu0qq*8xlfw+=28k!yq&01!3djk7xz2b5j8kj+%sog'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
